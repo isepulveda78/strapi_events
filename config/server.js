67ee5d5688b39bdb1c02e5edcfb5b92a7f60ff8d
Sepulveda <sepulvi@gcsnc.com>
2021-12-07 15:57:48 -0500

    saving

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e8960a548cc475203d79c54a5ada467'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fc416bbe-c6ca-4ecd-a5df-fbdb161af9a1'
+};
